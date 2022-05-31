d1ae9859174508f97b5510623d1146944d71f905
unknown <awesomeman4455@jandex.ru>
2022-05-31 16:54:19 +0200

    Mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
 
 let popUp = document.querySelector('.popup');//находим Pop-up окно
 let popUpOpen = document.querySelector('.profile__edit-button');//находим Edit button
-let popUpClose = document.querySelector('.form__btn-close');//находим Close button
+let popUpClose = document.querySelector('.popup__btn-close');//находим Close button
 
 let authorName = document.querySelector('.profile__title');//находим имя автора на стр.
 let authorProfession = document.querySelector('.profile__subtitle');//находим профессию на стр.
-let authorNameEdit = document.querySelector('.form__input-name'); // находим поле Имя
-let authorProfessionEdit = document.querySelector('.form__input-profession');// находим поле Профессия
+let authorNameEdit = document.querySelector('.popup__input_type_name'); // находим поле Имя
+let authorProfessionEdit = document.querySelector('.popup__input_type_profession');// находим поле Профессия
 
-let formSubmit = document.querySelector('.form');
+let formSubmit = document.querySelector('.popup__form');
 
 function openPopUp() { //функция открытия
     popUp.classList.add('popup_open');//добавляем класс 
